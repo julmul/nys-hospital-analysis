2137fcadae9c6134c78e366236a05e075c864c08
Julia <84592416+julmul@users.noreply.github.com>
2024-12-07 12:43:57 -0500

    Added comments to data cleaning script

--- scripts/tidy_hospital_data.R
+++ scripts/tidy_hospital_data.R
@@ -4,15 +4,18 @@ source('scripts/utils.R')
 
 data <- read_csv('source_data/NYS_hospital_discharges_2022_20241004.csv')
 
+# Ensure all column names are lowercase with no spaces
 col_names <- names(data) %>%
   str_to_lower(.) %>%
   str_replace_all(., ' ',  '_')
 names(data) <- col_names
 
+# Create admission ID column and tidy column names
 data <- data %>%
   rowid_to_column(.) %>%
   rename(zip_code = `zip_code_-_3_digits`,
          admission_id = rowid)
 
+# Save cleaned data set
 ensure_directory('derived_data')
 write_csv(data, 'derived_data/hospital_discharges_tidied.csv')
