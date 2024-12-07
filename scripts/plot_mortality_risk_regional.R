415cb33982e3a08f51e51258500bf37349c128d9
Julia <84592416+julmul@users.noreply.github.com>
2024-12-06 20:49:12 -0500

    Updated scripts to ensure directories before saving

--- scripts/plot_mortality_risk_regional.R
+++ scripts/plot_mortality_risk_regional.R
@@ -1,10 +1,21 @@
 library(tidyverse)
 library(RSQLite)
 
+source('scripts/utils.R')
+
 conn <- dbConnect(RSQLite::SQLite(), dbname = 'derived_data/hospital-discharges.db')
 hospitals <- dbGetQuery(conn, 'SELECT admission_id, hospital_service_area FROM hospital')
 clinical <- dbGetQuery(conn, 'SELECT admission_id, apr_risk_of_mortality FROM clinical')
 dbDisconnect(conn)
+
+# Identify number of patients with data on risk of mortality
+rom_total <- clinical %>% nrow()
+rom_nonmissing <- clinical %>% filter(!is.na(apr_risk_of_mortality)) %>% nrow()
+
+# Write counts to separate .txt files
+ensure_directory('logs')
+writeLines(as.character(rom_total), 'logs/total_rom_count.txt')
+writeLines(as.character(rom_nonmissing), 'logs/total_rom_nonmissing.txt')
 
 # Calculate proportions of mortality risk classifications by region
 region_mortality_risk <- clinical %>%
@@ -24,9 +35,10 @@ ggplot(region_mortality_risk, aes(x = hospital_service_area, y = proportion, fil
   labs(x = 'Hospital Service Area',
        y = 'Proportion of Patients',
        fill = 'Mortality Risk') +
-  theme_minimal(base_size = 16) +
+  theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_fill_manual(values = c('#2ca02c', 'blue', '#ff7f0e', '#d62728'))
 
 # Save figure
+ensure_directory('figures')
 ggsave('figures/regional_mortality_risk_proportions.png', height = 5, width = 7)
