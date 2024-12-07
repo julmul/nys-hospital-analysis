01e6a5bc31751865c12219fe161d45efe57b25a6
Julia <84592416+julmul@users.noreply.github.com>
2024-12-07 12:20:02 -0500

    Added k-means clustering analysis of hospital risk

--- Makefile
+++ Makefile
@@ -12,33 +12,46 @@ clean:
 	rm -rf logs
 	rm report.pdf
 
-derived_data/hospital_discharges_tidied.csv: source_data/NYS_hospital_discharges_2022_20241004.csv scripts/tidy_hospital_data.R
+derived_data/hospital_discharges_tidied.csv: source_data/NYS_hospital_discharges_2022_20241004.csv scripts/tidy_hospital_data.R scripts/utils.R
 	Rscript scripts/tidy_hospital_data.R
 
 derived_data/hospital-discharges.db: derived_data/hospital_discharges_tidied.csv scripts/create_sql_db.R
 	Rscript scripts/create_sql_db.R
 
-figures/hospitals_per_county.png: derived_data/hospital-discharges.db scripts/map_hospitals_by_county.R
+figures/hospitals_per_county.rds: derived_data/hospital-discharges.db source_data/Counties_Shoreline.shp scripts/map_hospitals_by_county.R scripts/utils.R
 	Rscript scripts/map_hospitals_by_county.R
+
+figures/nys_hospital_service_areas.rds: derived_data/hospital-discharges.db source_data/Counties_Shoreline.shp scripts/map_hospital_service_areas.R scripts/utils.R
+	Rscript scripts/map_hospital_service_areas.R
 	
-figures/regional_mortality_risk_proportions.png: derived_data/hospital-discharges.db scripts/plot_mortality_risk_regional.R
+figures/regional_mortality_risk_proportions.png logs/total_rom_nonmissing.txt logs/total_rom_count.txt: derived_data/hospital-discharges.db scripts/plot_mortality_risk_regional.R scripts/utils.R
 	Rscript scripts/plot_mortality_risk_regional.R
 	
-figures/regional_mortality_odds.png: derived_data/hospital-discharges.db scripts/model_extreme_mortality_risk.R
+figures/regional_mortality_odds.png: derived_data/hospital-discharges.db scripts/model_extreme_mortality_risk.R scripts/utils.R
 	Rscript scripts/model_extreme_mortality_risk.R
+
+figures/top_10_diagnosis_overall.png: derived_data/hospital-discharges.db scripts/plot_top_10_dx.R scripts/utils.R
+	Rscript scripts/plot_top_10_dx.R
 	
-figures/top_10_diagnosis_deceased_pts.png logs/total_pt_count.txt logs/total_deceased_count.txt: derived_data/hospital-discharges.db scripts/plot_causes_of_death.R
+figures/top_10_diagnosis_deceased_pts.png logs/total_pt_count.txt logs/total_deceased_count.txt: derived_data/hospital-discharges.db scripts/plot_causes_of_death.R scripts/utils.R
 	Rscript scripts/plot_causes_of_death.R
 
-figures/relative_predictive_importance.rds: derived_data/hospital-discharges.db scripts/predict_mortality_risk.R
+figures/relative_predictive_importance.rds: derived_data/hospital-discharges.db scripts/predict_mortality_risk.R scripts/utils.R
 	Rscript scripts/predict_mortality_risk.R
+
+figures/hospital_clusters_high_risk.png figures/nys_map_hospital_clusters.rds: derived_data/hospital-discharges.db scripts/cluster_hospital_risk.R scripts/utils.R
+	Rscript scripts/cluster_hospital_risk.R
 
 report.pdf: report.Rmd\
  init\
- figures/hospitals_per_county.png\
+ figures/hospitals_per_county.rds\
+ figures/nys_hospital_service_areas.rds\
  figures/regional_mortality_risk_proportions.png\
  figures/regional_mortality_odds.png\
+ figures/top_10_diagnosis_overall.png\
  figures/top_10_diagnosis_deceased_pts.png\
- figures/relative_predictive_importance.rds
+ figures/relative_predictive_importance.rds\
+ figures/hospital_clusters_high_risk.png\
+ figures/nys_map_hospital_clusters.rds
 	Rscript -e "rmarkdown::render('report.Rmd', output_format='pdf_document')"
 	
---
+++ scripts/cluster_hospital_risk.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(RSQLite)
+library(sf)
+
+source('scripts/utils.R')
+
+conn <- dbConnect(RSQLite::SQLite(), dbname = 'derived_data/hospital-discharges.db')
+hospital <- dbGetQuery(conn, 'SELECT admission_id, facility_name, permanent_facility_id, hospital_service_area, hospital_county FROM hospital')
+clinical <- dbGetQuery(conn, 'SELECT admission_id, length_of_stay, apr_risk_of_mortality, apr_severity_of_illness_code FROM clinical')
+payment <- dbGetQuery(conn, 'SELECT admission_id, total_charges FROM payment')
+dbDisconnect(conn)
+
+nys <- read_sf('source_data/Counties_Shoreline.shp')
+gps <- read_csv('source_data/hospital_gps_coords.csv')
+
+# Combine and clean the data
+data <- hospital %>%
+  full_join(clinical, by = 'admission_id') %>%
+  full_join(payment, by = 'admission_id') %>%
+  filter(complete.cases(.)) %>% 
+  mutate(apr_risk_of_mortality_code = case_when(
+      apr_risk_of_mortality == 'Minor' ~ 1,
+      apr_risk_of_mortality == 'Moderate' ~ 2,
+      apr_risk_of_mortality == 'Major' ~ 3,
+      apr_risk_of_mortality == 'Complete' ~ 4))
+
+# Summarize data at the hospital level
+hospital_data <- data %>%
+  group_by(permanent_facility_id) %>%
+  summarize(
+    avg_apr_risk_of_mortality = mean(apr_risk_of_mortality_code, na.rm = TRUE),
+    avg_apr_severity_of_illness = mean(apr_severity_of_illness_code, na.rm = TRUE),
+    avg_length_of_stay = mean(length_of_stay, na.rm = TRUE),
+    avg_total_charges = mean(total_charges, na.rm = TRUE))
+
+# Scale hospital data and perform k-means clustering
+hospital_data_scaled <- scale(hospital_data[ , -1])
+set.seed(123)
+kmeans_result <- kmeans(hospital_data_scaled, centers = 2, nstart = 25)
+hospital_data$high_risk_cluster <- kmeans_result$cluster
+
+# Visualize hospital clusters
+plt <- ggplot(hospital_data, aes(x = avg_length_of_stay, y = avg_apr_risk_of_mortality, color = factor(high_risk_cluster))) +
+  geom_point() +
+  labs(x = 'Average Length of Stay',
+       y = 'Average APR Risk of Mortality',
+       color = 'Cluster') +
+  theme_minimal()
+
+# Check major high-risk outliers
+data %>% filter(permanent_facility_id == '001138') %>% distinct(facility_name, hospital_county) # Blythedale Children's Hospital (Westchester)
+data %>% filter(permanent_facility_id == '001486') %>% distinct(facility_name, hospital_county) # Henry J. Carter Specialty Hospital (Manhattan)
+data %>% filter(permanent_facility_id == '001175') %>% distinct(facility_name, hospital_county) # Calvary Hospital Inc (Bronx)
+data %>% filter(permanent_facility_id == '010223') %>% distinct(facility_name, hospital_county) # Calvary Hospital.(Kings)
+
+# Summarize cluster statistics
+aggregate(cbind(avg_apr_risk_of_mortality, avg_apr_severity_of_illness, avg_length_of_stay) ~ high_risk_cluster, data = hospital_data, FUN = mean)
+
+# Merge GPS with hospital clusters
+gps <- gps %>%
+  st_as_sf(coords = c('longitude', 'latitude'), crs = 4326) %>%
+  left_join(hospital_data, by = 'permanent_facility_id') %>%
+  mutate(high_risk_cluster = if_else(high_risk_cluster == 1, 'Low-Risk', 'High-Risk'))
+
+# Visualize hospital clusters on map of NYS
+map <- ggplot() +
+  geom_sf(data = nys, color = 'black', fill = 'grey') +
+  geom_sf(data = gps, aes(fill = factor(high_risk_cluster)), shape = 21, stroke = 0.5) +
+  labs(fill = 'Hospital Cluster') +
+  theme_void()
+
+# Save figures
+ensure_directory('figures')
+ggsave('figures/hospital_clusters_high_risk.png', plt, height = 7, width = 7, dpi = 600)
+write_rds(map, 'figures/nys_map_hospital_clusters.rds')
